15a318251e600dd9be1507a7e87b4eecf2b55ed6
novichok271 <rustamutkirov3141@gmail.com>
2026-04-13 14:22:24 +0500

    Решил two sum

--- arrays/easy/001_two_sum.py
+++ arrays/easy/001_two_sum.py
@@ -1,30 +1,8 @@
-"""
-1. Two Sum
-https://leetcode.com/problems/two-sum/
-Difficulty: Easy
-Topic: Arrays
-
-Approach:
-- Проходим по массиву, для каждого элемента проверяем есть ли
-  дополнение (target - num) в словаре
-- Словарь хранит {значение: индекс}
-Time: O(n)
-Space: O(n)
-"""
-
 class Solution:
     def twoSum(self, nums: list[int], target: int) -> list[int]:
-        seen = {}
-        for i, num in enumerate(nums):
-            complement = target - num
-            if complement in seen:
-                return [seen[complement], i]
-            seen[num] = i
-        return []
-
-
-if __name__ == "__main__":
-    s = Solution()
-    print(s.twoSum([2, 7, 11, 15], 9))   # [0, 1]
-    print(s.twoSum([3, 2, 4], 6))         # [1, 2]
-    print(s.twoSum([3, 3], 6))            # [0, 1]
+        pair = dict()
+        for idx, num in enumerate(nums):
+            diff = target - num 
+            if diff in pair:
+                return [pair[diff], idx]
+            pair[num] = idx
