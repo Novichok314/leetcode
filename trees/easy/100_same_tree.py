649eb82908f48d0320b40ca12e40ef6197a89786
novichok271 <rustamutkirov3141@gmail.com>
2026-04-13 16:03:00 +0500

    1004 solved

--- trees/easy/100_same_tree.py
+++ trees/easy/100_same_tree.py
@@ -5,7 +5,7 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+    def isSameTree(self, p, q) -> bool:
         if not p and not q:
             return True
         if not p or not q:
