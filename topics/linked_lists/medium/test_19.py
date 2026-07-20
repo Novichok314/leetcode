2edfd00eee995b2adfd530dba7f3530044a2dd1b
Novichok314 <rustamutkirov1807@gmail.com>
2026-07-20 13:16:59 +0500

    Решил проблему с тестированием для 19й задачи

--- topics/linked_lists/medium/test_19.py
+++ topics/linked_lists/medium/test_19.py
@@ -1,9 +1,38 @@
-from topics.linked_lists.medium.problem_19 import ListNode, Solution
+from topics.linked_lists.medium.problem_19 import Solution, ListNode
 
 
-def test():
+def list_to_linked(values):
+    """Python list -> ListNode chain"""
+    dummy = ListNode()
+    current = dummy
+    for v in values:
+        current.next = ListNode(v)
+        current = current.next
+    return dummy.next
+
+def linked_to_list(node):
+    """ListNode chain -> Python list"""
+    result = []
+    while node:
+        result.append(node.val)
+        node = node.next
+    return result
+
+
+def test_remove_from_middle():
     solution = Solution()
-    assert solution.removeNthFromEnd(ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5))))), 2) == ListNode(1, ListNode(2, ListNode(3, ListNode(5))))
-    assert solution.removeNthFromEnd(ListNode(1), 1) == None
-    assert solution.removeNthFromEnd(ListNode(1, ListNode(2)), 1) == ListNode(1)
-    
+    head = list_to_linked([1, 2, 3, 4, 5])
+    result = solution.removeNthFromEnd(head, 2)
+    assert linked_to_list(result) == [1, 2, 3, 5]
+
+def test_remove_single_node():
+    solution = Solution()
+    head = list_to_linked([1])
+    result = solution.removeNthFromEnd(head, 1)
+    assert result is None
+
+def test_remove_first_of_two():
+    solution = Solution()
+    head = list_to_linked([1, 2])
+    result = solution.removeNthFromEnd(head, 2)
+    assert linked_to_list(result) == [2]
