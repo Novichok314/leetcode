f5834acb6ea08eb46fc859d09d1e7a88404de1a7
Novichok314 <rustamutkirov1807@gmail.com>
2026-07-22 22:40:10 +0500

    добавил тесты к задаче 76

--- topics/sliding_window/hard/test_76.py
+++ topics/sliding_window/hard/test_76.py
@@ -3,4 +3,7 @@ from topics.sliding_window.hard.problem_76 import Solution
 
 def test():
     solution = Solution()
-    assert solution.minWindow() is None
+    assert solution.minWindow("ADOBECODEBANC", "ABC") == "BANC"
+    assert solution.minWindow("a", "a") == "a"
+    assert solution.minWindow("a", "aa") == ""
+    assert solution.minWindow("ab", "b") == "b"
