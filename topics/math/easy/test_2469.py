3e0a461c3ba790f7448d3b784fbdcef71c3fff58
Novichok314 <rustamutkirov1807@gmail.com>
2026-07-22 23:04:18 +0500

    исправил ошибку сравнения чисел с плавающей точностью

--- topics/math/easy/test_2469.py
+++ topics/math/easy/test_2469.py
@@ -1,8 +1,9 @@
+import pytest
 from topics.math.easy.problem_2469 import Solution
 
 
 def test():
     solution = Solution()
-    assert solution.convertTemperature(30) == [303.15, 86.0]
-    assert solution.convertTemperature(0) == [273.15, 32.0]
-    assert solution.convertTemperature(-40) == [233.15, -40.0]
+    assert solution.convertTemperature(30) == pytest.approx([303.15, 86.0])
+    assert solution.convertTemperature(0) == pytest.approx([273.15, 32.0])
+    assert solution.convertTemperature(-40) == pytest.approx([233.15, -40.0])
